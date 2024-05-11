e8a3e89f628e6024ac3a785e806d4632ebac74a8
ThanhNT <63948294+ntthanh-99@users.noreply.github.com>
2024-05-11 17:58:35 +0700

    Update todo

--- Spring/day5/homework/lession5/src/main/java/com/thanhnt/lession5/Lession5Application.java
+++ Spring/day5/homework/lession5/src/main/java/com/thanhnt/lession5/Lession5Application.java
@@ -14,26 +14,27 @@ import java.util.concurrent.TimeUnit;
 @SpringBootApplication
 public class Lession5Application {
 
-	public static void main(String[] args) throws MqttException, InterruptedException {
-		SpringApplication.run(Lession5Application.class, args);
+    public static void main(String[] args) throws MqttException, InterruptedException {
+        SpringApplication.run(Lession5Application.class, args);
 
-		String publisherId = UUID.randomUUID().toString();
-		IMqttClient publisher = new MqttClient("tcp://iot.eclipse.org:1883",publisherId);
+        String publisherId = UUID.randomUUID().toString();
+        //IMqttClient publisher = new MqttClient("tcp://iot.eclipse.org:1883", publisherId);
+        IMqttClient publisher = new MqttClient("tcp://broker.hivemq.com:1883", publisherId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+        publisher.connect(options);
 
-		MqttConnectOptions options = new MqttConnectOptions();
-		options.setAutomaticReconnect(true);
-		options.setCleanSession(true);
-		options.setConnectionTimeout(10);
-		publisher.connect(options);
+        CountDownLatch receivedSignal = new CountDownLatch(10);
+        publisher.subscribe(EngineTemperatureSensor.TOPIC, (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            // ... payload handling omitted
+            System.out.println(topic);
+            System.out.println(new String(payload));
 
-		CountDownLatch receivedSignal = new CountDownLatch(10);
-		publisher.subscribe(EngineTemperatureSensor.TOPIC, (topic, msg) -> {
-			byte[] payload = msg.getPayload();
-			System.out.println(payload);
-			// ... payload handling omitted
-			receivedSignal.countDown();
-		});
-		receivedSignal.await(1, TimeUnit.MINUTES);
-	}
-
+            receivedSignal.countDown();
+        });
+        receivedSignal.await(1, TimeUnit.MINUTES);
+    }
 }
